5ead1c16fb20b541910d9f695ed5443ade6b501d
dylsteck <dylancsteck@gmail.com>
2025-11-11 10:40:19 -0500

    chore: bump deps

--- components/view-as-miniapp.tsx
+++ components/view-as-miniapp.tsx
@@ -1,11 +1,12 @@
 "use client";
 
-import { APP_URL } from "@/constants";
+import { APP_DOMAIN, APP_URL } from "@/constants";
 import { Cuer } from "cuer";
 
 export default function ViewAsMiniApp() {
 
-  const baseMiniAppUrl = `cbwallet://miniapp?url=${encodeURIComponent(APP_URL)}`;
+  const appUrl = APP_URL ?? `https://${APP_DOMAIN}`;
+  const baseMiniAppUrl = `cbwallet://miniapp?url=${encodeURIComponent(appUrl)}`;
 
   return (
     <div>
